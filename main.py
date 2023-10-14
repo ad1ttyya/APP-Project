3a53f1db3a4c18c03d54e0d240a13e21fefc26b4
Aditya Bakshi <adinmadi@gmail.com>
2023-10-14 18:15:06 +0530

    Cart added

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/cart')
+def index():
+    return render_template('cart.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
