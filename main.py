f1a7bfc716e38eb951a521a92a3f25fbff8a7880
Aditya Bakshi <adinmadi@gmail.com>
2023-10-30 22:18:03 +0530

    Deploying

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 
+class Prod_listing(db.Model):
+    Listing_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    Prod_Name: Mapped[str] = mapped_column(String)
+    Prod_Price: Mapped[str] = mapped_column(String)
+    Prod_Img: Mapped[str] = mapped_column(Integer)
+
+
+db = SQLAlchemy(model_class=Prod_listing)
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql:root:root@localhost/shopez'
+
+db.init_app(app)
+
+
 
 @app.route('/')
 def index():
