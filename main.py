487f8e50d868f0e0a66c5ea2592e1d8a681b1d01
Aditya Bakshi <adinmadi@gmail.com>
2023-12-14 17:58:17 +0530

    Page linking

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from sqlalchemy.orm import DeclarativeBase
 from databasecon import *
 app = Flask(__name__)
 get_listing()
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
     return render_template('index.html',items=items)
-@app.route('/cart')
+@app.route('/cart', methods = ['GET', 'POST'])
 def cart():
     return render_template('cart.html')
-@app.route('/item')
+@app.route('/item', methods = ['GET', 'POST'])
 def item():
     return render_template('Item.html')
 if __name__ == "__main__":
